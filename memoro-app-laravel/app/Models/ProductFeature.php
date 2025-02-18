dc5ed06b11568e1fbaf9f3d6906bc0d64e1ac60b
Arthur Detomi <arthurdetomi@gmail.com>
2025-02-18 10:46:44 -0300

    release: Adding models and mapping product routes

---
+++ memoro-app-laravel/app/Models/ProductFeature.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductFeature extends Model
+{
+    // TODO: Deve-se adicionar uma chave primária e deixar as duas como unique ao inves de usar chave composta
+}
--- memoro-app-laravel/routes/web.php
+++ memoro-app-laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Products routes
+Route::resource('products', ProductController::class);
