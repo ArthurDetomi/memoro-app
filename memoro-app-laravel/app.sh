c9d9f554f98ebf859b1485664de9bbd8509f1119
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-10 20:05:28 -0300

    release: Dockerization in progress

---
+++ memoro-app-laravel/app.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt update && apt install -y curl unzip default-mysql-client
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+docker-php-ext-install pdo pdo_mysql mbstring zip gd
+
+chown -R www-data:www-data /memoro-app-laravel/storage
+chown -R www-data:www-data /memoro-app-laravel/bootstrap/cache
+chmod -R 775 /memoro-app-laravel/storage
+chmod -R 775 /memoro-app-laravel/bootstrap/cache
+
+sleep 10
+
+composer install
+
+php artisan migrate
+
+php artisan config:cache
+php artisan route:cache
+
+php artisan key:generate
+
+php artisan storage:link
+
+# Inicia o servidor Laravel
+php artisan serve --host=0.0.0.0 --port=80
