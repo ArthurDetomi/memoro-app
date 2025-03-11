8f4ae5d01e99289e4a406bba5f58b630887b3ac2
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-11 18:38:37 -0300

    fix: restore settings

--- memoro-app-laravel/app.sh
+++ memoro-app-laravel/app.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-apt update && apt install -y curl unzip git default-mysql-client php-zip
+apt update && apt install -y curl unzip default-mysql-client
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 docker-php-ext-install pdo pdo_mysql mbstring zip gd
 
-chown -R www-data:www-data /memoro-app-laravel/storage
-chown -R www-data:www-data /memoro-app-laravel/bootstrap/cache
-chown -R www-data:www-data /memoro-app-laravel/vendor
+chown -R www-data:www-data /ideas-project/storage
+chown -R www-data:www-data /ideas-project/bootstrap/cache
+chmod -R 775 /ideas-project/storage
+chmod -R 775 /ideas-project/bootstrap/cache
 
-chmod -R 775 /memoro-app-laravel/storage
-chmod -R 775 /memoro-app-laravel/bootstrap/cache
-chmod -R 775 /memoro-app-laravel/vendor
+sleep 20
 
-sleep 10
-
-composer install --no-interaction --prefer-dist -vvv
+composer install
 
 php artisan migrate
 
 php artisan config:cache
 php artisan route:cache
-php artisan key:generate
-php artisan storage:link# Inicia o servidor Laravel
 
+php artisan key:generate
+
+php artisan storage:link
+
+# Inicia o servidor Laravel
 php artisan serve --host=0.0.0.0 --port=80
