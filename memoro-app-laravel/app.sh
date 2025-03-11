cab20530a7e469836fd92920c4acda99318d72fc
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-11 19:16:58 -0300

    Fix: New test

--- memoro-app-laravel/app.sh
+++ memoro-app-laravel/app.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
-apt update && apt install -y curl unzip default-mysql-client && \
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer && \
-docker-php-ext-install pdo pdo_mysql mbstring zip gd && \
-chown -R www-data:www-data /memoro-app-laravel/storage && \
-chown -R www-data:www-data /memoro-app-laravel/bootstrap/cache && \
-chmod -R 775 /memoro-app-laravel/storage && \
+apt update && apt install -y curl unzip default-mysql-client
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+docker-php-ext-install pdo pdo_mysql mbstring zip gd
+
+chown -R www-data:www-data /memoro-app-laravel/storage
+chown -R www-data:www-data /memoro-app-laravel/bootstrap/cache
+chmod -R 775 /memoro-app-laravel/storage
 chmod -R 775 /memoro-app-laravel/bootstrap/cache
 
 sleep 10
 
-echo "Executando composer"
-composer clear-cache
 composer install
-echo "Finalizando execução do composer..."
 
 php artisan migrate
 
