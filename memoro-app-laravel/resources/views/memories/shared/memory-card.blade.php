4a047431bba73a10330678a95ac2afc3ee0f418a
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-17 15:48:32 -0300

    release: Removendo do front-end tudo que ainda não foi implementado

--- memoro-app-laravel/resources/views/memories/shared/memory-card.blade.php
+++ memoro-app-laravel/resources/views/memories/shared/memory-card.blade.php
@@ -69,16 +69,7 @@
 
         <!-- Post Footer Starts -->
         <div class="row my-3">
-            <div class="col">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-chat-quote" viewBox="0 0 16 16">
-                    <path
-                        d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                    <path
-                        d="M7.066 6.76A1.665 1.665 0 0 0 4 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112zm4 0A1.665 1.665 0 0 0 8 7.668a1.667 1.667 0 0 0 2.561 1.406c-.131.389-.375.804-.777 1.22a.417.417 0 0 0 .6.58c1.486-1.54 1.293-3.214.682-4.112z" />
-                </svg>
-                <small>Comments(9)</small>
-            </div>
+            <!-- No futuro pode adicionar comentários -->
 
             <div class="col text-end">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -88,19 +79,6 @@
                         d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                 </svg>
                 <small>Published Date: {{ $memory->created_at->toDateString() }}</small>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-1">
-                <img src="{{ $memory->user->getImageUrl() }}" alt="User1 Full Name" style="height: 25px" />
-            </div>
-
-            <div class="col-11">
-                <form action="#">
-                    <textarea name="add_comment" placeholder="Type your comment.." class="form-control mb-3" id=""></textarea>
-                    <input type="submit" name="submit" value="Add Comment" class="form-control btn btn-dark btn-sm" />
-                </form>
             </div>
         </div>
         <!-- Post Footer Ends -->
