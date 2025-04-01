3b9a4a100969f7f0045d17dfcff82b51ab1687be
Arthur Detomi <arthurdetomi@gmail.com>
2025-04-01 09:20:59 -0300

    Criando formulário para cadastro de memórias
    A ideia é remover requisições assincronas e usar apenas o padrão do blade

---
+++ memoro-app-laravel/resources/views/memories/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.layout')
+
+@section('title', 'Memory Create')
+
+@section('content')
+    <!-- Main Content Section Starts Here -->
+    <section class="main-content py-4">
+        <div class="container">
+            <div class="row">
+                @include('users.shared.left-sidebar-user-profile')
+
+                <!-- Feed Section Starts -->
+                <div class="col-md-9">
+                    <!-- Colocar formulário de cadastro -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Main Content Section Ends Here -->
+@endsection
