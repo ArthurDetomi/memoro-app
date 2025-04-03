d93abf08143095c61be40d57c3e78411d7fa155b
Arthur Detomi <arthurdetomi@gmail.com>
2025-04-02 22:29:53 -0300

    release: Criando models e migrations

---
+++ memoro-app-laravel/database/migrations/2025_04_02_164649_create_reviews_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false);
+            $table->foreignId('product_type_id')->nullable()->constrained(table: 'products_types', indexName: 'reviews_products_types_id')->cascadeOnDelete();
+            $table->boolean('is_default')->nullable(false)->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
