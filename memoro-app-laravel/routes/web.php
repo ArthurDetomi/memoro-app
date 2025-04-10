2de45dfbac36b548d9ba1e3e90a1ee39f5b65adf
Arthur Detomi <arthurdetomi@gmail.com>
2025-04-10 09:29:55 -0300

    fix: Correção de erro de redirecionamento para rota indefinida

--- memoro-app-laravel/routes/web.php
+++ memoro-app-laravel/routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\ProductReviewController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('memories.index');
-})->middleware('auth');
+Route::get('/', [MemoryController::class, 'index'])->middleware('auth');
 
 
 // Products routes
